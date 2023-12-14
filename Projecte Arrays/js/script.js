616b12eb4a41f4fae75a787dcae4ce06deaef0d3
Adria <adriagp90@gmail.com>
2023-12-14 18:54:13 +0100

    afegit inputs radio per selecció a l'hora de executar searchlist i implementació de la mateixa

--- Projecte Arrays/js/script.js
+++ Projecte Arrays/js/script.js
@@ -4,6 +4,8 @@
 */
 
 let longitud = 1000;
+
+let dades = [];
 let pokemons = [];
 let municipis = [];
 let meteorites = [];
@@ -55,21 +57,28 @@ fetch("js/data/movies.json")
 
 function mostraResults() {
 	console.clear();
-	let dades = [];
 	for (let i=0; i<longitud; i++) {
 		dades.push([pokemons[i], municipis[i], movies[i], meteorites[i]]);
 	}
 	console.table(dades);
 }
 
-// funció orderList que rep string per paràmetre (asc o desc) i retorna l'array ordenada
+// funció orderList que rep string per paràmetre (asc o desc) i mostra l'array ordenada
 function orderList(order) {
 	order == "asc" ? ordenaAsc() : ordenaDesc();
 	mostraResults();
 }
 // funció que retorna la posició d'un element buscat mitjantçant un prompt
 function searchList() {
-
+	let type = document
+	let name = prompt("Introdueix un nom a buscar").toUpperCase();
+	let match = [];
+	// dades.forEach((item) => item.forEach(
+	// 	(x) => {
+	// 		if(x != undefined && x.toUpperCase().includes(name)) {
+	// 		match.push(x); 
+	// 	}}));
+	console.table(match);
 }
 // funcio que calculi la mitjana d'un valor numèric (fixar a dos decimals amb toFixed(2))
 function calcMitjana() {
@@ -78,13 +87,14 @@ function calcMitjana() {
 // funció que crea la taula i mitjançant el DOM mostra el resultat en el div "resultat"
 function printList() {
 }
-
+// funció per ordenar de forma ascedent (a-z) els valors de l'array
 function ordenaAsc() {
 	pokemons.sort();
 	municipis.sort();
 	movies.sort();
 	meteorites.sort();
 }
+//funció per ordenar de forma descendent (z-a) els valors de l'array
 function ordenaDesc() {
 	ordenaAsc();
 	pokemons.reverse();
